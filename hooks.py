2b758193b3c7189d6940d34424f0c558b5cc8b13
Adrian Cederberg <adrn.cederberg123@gmail.com>
2024-07-05 12:37:27 -0600

    feature(content): Added page for ``/text``.
    Because setting this up in traefik sucks.

--- hooks.py
+++ hooks.py
@@ -8,7 +8,18 @@ def captura_plugins_app(app_view: Type[AppView]):
     """The text extension is required!"""
 
     from acederbergio import ColorView
+    from app.depends import DependsSessionMaker
+    from text_app import depends
     from text_app.router import TextView
+
+    @app_view.view_router.get("/text")
+    def get_home(
+        sessionmaker: DependsSessionMaker,
+        status: depends.DependsTextBuilderStatus,
+        template: depends.DependsTemplate,
+    ):
+        data = depends.get_by_name_json(sessionmaker, status, name="home")  # type: ignore
+        return depends.get_by_name_text(data, template, "home")  # type: ignore
 
     app_view.view_router.include_router(TextView.view_router, prefix="/text")
     app_view.view_router.include_router(ColorView.view_router, prefix="/colors")
